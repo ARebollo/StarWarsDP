688949f355b707d913566e09fa03e6b4d8196b45
ARebollo <muaddib58@gmail.com>
2016-06-22 04:18:02 +0200

    Add files via upload

---
+++ Pruebas/PruebasEstacion.java
@@ -0,0 +1,41 @@
+package Pruebas;
+
+import DEV.Estacion;
+import Estructuras.Midi;
+
+/**
+* Declaracion de la clase pruebasEstacion
+* @author
+*   <b> Antonio Rebollo Guerra, Carlos Salguero Sanchez </b><br>
+*   <b> Asignatura Desarrollo de Programas</b><br>
+*   <b> Curso 15/16 </b>
+*/
+public class PruebasEstacion {
+	
+	public static void ejecutarPruebas(){
+		
+		System.out.println("-----------------------------------");
+		
+		System.out.println("INICIANDO PRUEBAS Estacion.java:");
+		System.out.println();
+		
+		Estacion estPrueba = new Estacion();
+		
+		System.out.println("Comprobando aniadirMidi y mostrarLista (se añaden 3,7,11,5 y deberia devolverlos ordenados de menor a mayor):   ");
+		estPrueba.aniadirMidi(new Midi(3));
+		estPrueba.aniadirMidi(new Midi(7));
+		estPrueba.aniadirMidi(new Midi(11));
+		estPrueba.aniadirMidi(new Midi(5));
+		estPrueba.mostrarLista();
+		
+		System.out.println();		
+		System.out.println("-----------------------------------");
+		
+	}
+	
+	public static void main(String [ ] args){
+		
+		ejecutarPruebas();
+	}
+
+}
